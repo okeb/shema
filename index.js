c1b20a7cd9e563946f0b49ecc7eced800924f8bb
Oliver KEB <oliver.keb@icloud.com>
2023-01-11 20:11:23 +0100

    ✨ feat(verset):
    - ajout de la possibilité d'ecrire des intervalle de versets
    
    - changement des algorythme de recuperation de verset
    
    - refactoring
    
    - debut d'implementation de page d'accueil

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-const TOKEN = process.env.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN";
-var nom_du_livre = "";
-var chapitre = 1;
-var a = 0;
-var z = "";
-var abbr_list = ["Ge. ", "Ex. ", "Lé. ", "No. ", "De. ", "Jos. ", "Jg. ", "1 S. ", "2 S. ", "1 R. ", "2 R. ", "Es. ", "Jé. ", "Ez. ", "Os. ", "Joë. ", "Am. ", "Ab. ", "Jon. ", "Mi. ", "Na. ", "Ha. ", "So. ", "Ag. ", "Za. ", "Mal. ", "Ps. ", "Pr. ", "Job ", "Ca. ", "Ru. ", "La. ", "Ec. ", "Est. ", "Da. ", "Esd. ", "Né. ", "1 Ch. ", "2 Ch. ", "Mt. ", "Mc. ", "Lu. ", "Jn. ", "Ac. ", "Ja. ", "Ga. ", "1 Th. ", "2 Th. ", "1 Co. ", "2 Co. ", "Ro. ", "Ep. ", "Ph. ", "Col. ", "Phm. ", "1 Ti. ", "Tit. ", "1 Pi. ", "2 Pi. ", "2 Ti. ", "Jud. ", "Hé. ", "1 Jn. ", "2 Jn. ", "3 Jn. ", "Ap. "]
+// const TOKEN = process.env.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN";
+const abbr_list = ["Ge. ", "Ex. ", "Lé. ", "No. ", "De. ", "Jos. ", "Jg. ", "1 S. ", "2 S. ", "1 R. ", "2 R. ", "Es. ", "Jé. ", "Ez. ", "Os. ", "Joë. ", "Am. ", "Ab. ", "Jon. ", "Mi. ", "Na. ", "Ha. ", "So. ", "Ag. ", "Za. ", "Mal. ", "Ps. ", "Pr. ", "Job ", "Ca. ", "Ru. ", "La. ", "Ec. ", "Est. ", "Da. ", "Esd. ", "Né. ", "1 Ch. ", "2 Ch. ", "Mt. ", "Mc. ", "Lu. ", "Jn. ", "Ac. ", "Ja. ", "Ga. ", "1 Th. ", "2 Th. ", "1 Co. ", "2 Co. ", "Ro. ", "Ep. ", "Ph. ", "Col. ", "Phm. ", "1 Ti. ", "Tit. ", "1 Pi. ", "2 Pi. ", "2 Ti. ", "Jud. ", "Hé. ", "1 Jn. ", "2 Jn. ", "3 Jn. ", "Ap. "]
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require('morgan');
+const environment = "dev"
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -230,114 +227,179 @@ function get_book_name(param){
   return nom_du_livre
 }
 
+
 /**
- * permet de recuperer les textes en fonction des paramètres transmis
- * @param {*} res reponse du serveur
- * @param {string} nom_livre nom du livre de la bible dont on souhaite recupérer le(s) verset(s)
- * @param {string|integer} chapitre chapitre du livre
- * @param {string|integer} a numéro du verset de depart
- * @param {string|integer} z numéro du verset final (optionel)
- * @returns {JSON} un json contenant les versets choisi de la bible
+ * Recupère un livre entier
+ * @param {string} nom_du_livre nom du livre a récuperer
+ * @param {string} action type de données que l'on souhaite obtenir ("all", "info", "")
  */
-function get_selection(res, nom_livre, chapitre, a, z) {
+function get_book(nom_du_livre, action) {
   const bym = require("./db/thebym.json");
-  resultats = {};
-  var y = 0;
-  var has_z = true;
-  var has_a = true;
-  var has_chap = true;
+  const livre = get_book_name(nom_du_livre);
+  var result = {}
+
+  if (typeof action === "undefined") {
+    action = "verset"
+  }
+
+  switch (action) {
+    case "verset":
+      var num_chap = 1;
+      var a = 1;
+      var verset_actual = {};
+      var search = true;
+
+      while (search) {
+        while (search) {
+          a++;
+          var verset = livre + num_chap + ":" + a;
+          var ecriture = bym[verset];
+
+          if (ecriture) {
+            verset_actual = {};
+            verset_actual["livre"] = livre;
+            verset_actual["chapitre"] = num_chap;
+            verset_actual["verset"] = a;
+            verset_actual["ecrit"] = ecriture;
+            verset_actual["version"] = "Bible de Yéhoshoua Ha Mashiah";
+            result[verset] = verset_actual;
+          }else{
+            search = false;
+          }
+        }
+        num_chap++;
+        if (a > 0) {
+          a = 0;
+          search = true;
+        }
+        if (!bym[livre + num_chap + ":1"]) {
+          search = false;
+        }
+      }
+      break;
+  
+    default:
+      break;
+  }
+  
+  return result;
+}
+
+
+/**
+ * recupère tout le chapitre d'un livre de la bible
+ * @param {striong} nom_du_livre nom du livre
+ * @param {string} chapitre chapitre du livre
+ * @returns {JSON} versets du chapitre du livre demandé
+ */
+function get_all_chapter(nom_du_livre, chapitre){
+  const bym = require("./db/thebym.json");
+  const livre = get_book_name(nom_du_livre);
+  var result = {};
+  var a = 0
+  var continued = true;
+
+  while (continued) {
+    a++;
+    var verset = livre + chapitre + ":" + a;
+    var ecriture = bym[verset];
+
+
+    if (ecriture) {
+      var verset_actual = {};
+      verset_actual["livre"] = livre;
+      verset_actual["chapitre"] = chapitre;
+      verset_actual["verset"] = verset;
+      verset_actual["ecrit"] = ecriture;
+      verset_actual["version"] = "Bible de Yéhoshoua Ha Mashiah";
+      result[verset] = verset_actual;
+    } else {
+      continued = false;
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * crée un tableau contenant tous les numéros de verset contenu dans le params
+ * @param {string} versets liste des verset qu'on souhaite recupérer
+ * @returns selection tableau contenant la liste des numero des versets
+ */
+function make_selection(versets){
+  var selection = []
+
+  const listes = versets.split(",");
+  for (let i = 0; i < listes.length; i++) {
+    const liste = listes[i];
+    if(liste.includes("-")){
+      const v_debut = parseInt(liste.split("-")[0])
+      const v_fin = parseInt(liste.split("-")[1]);
+      for (var y = v_debut; y <= v_fin; y++) {
+        selection.push(y);
+      }
+
+    }else{
+      selection.push(parseInt(liste));
+    } 
+  }
+
+  selection.sort((a, b) =>(a - b))
+
+  // console.log(selection)
+  return selection
+}
+
+
+/**
+ * recupere tous les versets selectionnés
+ * @param {string} nom_livre c'est le nom du livre dans lequel on va récuperer les verset
+ * @param {string} chapitre c'est le nom du chapitre du livre 
+ * @param {array} notre_selection un tableau de tous les versets selectionnés
+ * @returns {JSON} un json contenant les versets choisi de la bible 
+ */
+function get_all_of_selection(nom_livre, chapitre, notre_selection) {
+  const bym = require("./db/thebym.json");
+  const selection = make_selection(notre_selection);
+  var result = {};
 
   nom_du_livre = get_book_name(nom_livre);
 
-  if (z === "" || z === 0 || typeof z === "undefined") {
-    has_z = false;
-  } else {
-    z = parseInt(z);
-  }
+  for (let x = 0; x < selection.length; x++) {
+    const num_verset = selection[x];
+    const v_name = nom_du_livre + chapitre + ":" + num_verset;
+    v_value = bym[v_name];
+    if (typeof v_value !== "undefined") {
+      var verset_actual = {};
 
-  if (a === "" || a === 0 || typeof a === "undefined") {
-    a = 1;
-    x = 1;
-    has_a = false;
-  } else {
-    a = parseInt(a);
-    x = parseInt(a);
-  }
+      verset_actual["livre"] = nom_du_livre;
+      verset_actual["chapitre"] = chapitre;
+      verset_actual["verset"] = num_verset;
+      verset_actual["ecrit"] = v_value;
+      verset_actual["version"] = "Bible de Yéhoshoua Ha Mashiah";
 
-  if (chapitre === "" || typeof chapitre === "undefined") {
-    has_chap = false;
-    y = 1;
-    chapitre = y;
-    var next_c = y;
-  }
-
-  var v_name = nom_du_livre + chapitre + ":" + a;
-  var v_value = bym[v_name];
-
-
-  while (y !== "stop") {
-    while (x !== "stop") {
-      v_name = nom_du_livre + chapitre + ":" + x;
-      v_value = bym[v_name];
-
-      if (typeof v_value !== "undefined") {
-        var verset_actual = {};
-
-        verset_actual["livre"] = nom_du_livre;
-        verset_actual["chapitre"] = chapitre;
-        verset_actual["verset"]= x;
-        verset_actual["ecrit"] = v_value;
-        verset_actual["version"] = "Bible de Yéhoshoua Ha Mashiah";
-        resultats[v_name] = verset_actual;
-
-        // resultats[v_name] = v_value;
-        x++;
-
-        switch (true) {
-          case has_z && a <= x:
-            if (x > z) {
-              x = "stop";
-              y = "stop";
-            }
-            break;
-
-          case !has_z && a <= x:
-            if (has_chap) {
-              y = "stop";
-              if (has_a) {
-                x = "stop";
-              }
-            }
-            break;
-
-          default:
-            x = "stop";
-            break;
-        }
-      } else {
-        x = "stop";
-        if (has_z) {
-          y = "stop";
-        }
-      }
-    }
-
-    var next_c = parseInt(chapitre) + 1;
-    q = nom_du_livre + next_c + ":1";
-    v_value = bym[q];
-    if (typeof bym[q] !== "undefined" && !has_z && y !== "stop") {
-      chapitre = next_c;
-      x = 1;
-      y++;
-    } else {
-      y = "stop";
+      result[v_name] = verset_actual;
     }
   }
+  return result;
+}
 
+
+/**
+ * renvoi la reponse au client
+ * @param {response} res la reponse du serveur
+ * @param {JSON} result resultat a envoyer au client
+ */
+function return_result(res, result, environment) {
   try {
-    res.status(200).json(resultats);
+    res.status(200).json(result);
   } catch (err) {
-    console.error(err);
+    if (environment === "dev") {
+      console.error(err);
+    } else {
+      res.status(500);
+    }
   }
 }
 
@@ -356,7 +418,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
 
 
 // Récupére toute la bible
@@ -484,10 +545,12 @@ app.get("/bym", (req, res) => {
     "3 Jn. ": [], 
     "Ap. ": []
   }
+
+
   function find_verset(abbr_list){
     abbr_list = [
     "Ge. ", "Ex. ", "Lé. ", "No. ", "De. ", "Jos. ", "Jg. ", "1 S. ", "2 S. ", "1 R. ", "2 R. ", "Es. ", "Jé. ", "Ez. ", "Os. ", "Joë. ", "Am. ", "Ab. ", "Jon. ", "Mi. ", "Na. ", "Ha. ", "So. ", "Ag. ", "Za. ", "Mal. ", "Ps. ", "Pr. ", "Job ", "Ca. ", "Ru. ", "La. ", "Ec. ", "Est. ", "Da. ", "Esd. ", "Né. ", "1 Ch. ", "2 Ch. ", "Mt. ", "Mc. ", "Lu. ", "Jn. ", "Ac. ", "Ja. ", "Ga. ", "1 Th. ", "2 Th. ", "1 Co. ", "2 Co. ", "Ro. ", "Ep. ", "Ph. ", "Col. ", "Phm. ", "1 Ti. ", "Tit. ", "1 Pi. ", "2 Pi. ", "2 Ti. ", "Jud. ", "Hé. ", "1 Jn. ", "2 Jn. ", "3 Jn. ", "Ap. "
-  ]
+    ]
     return abbr_list[getRandomInt(abbr_list.length)] + getRandomInt(100) + ":" + getRandomInt(250)
   }
 
@@ -502,8 +565,9 @@ app.get("/bym", (req, res) => {
     verset: random_verset,
     ecrit: thebym[random_verset],
     version: "Bible de Yéhoshoua Ha Mashiah",
-    de: "https://www.shemaproject.org/bibleapi"
+    APIinfo: "https://www.shemaproject.org/bibleapi"
   };
+
   try {
     res.status(200).json(resultat);
   } catch (err) {
@@ -512,12 +576,20 @@ app.get("/bym", (req, res) => {
 });
 
 
-// Récupérer tous un livre
+// servir des fichiers statiques
+
+app.use("/home", express.static("src"));
+// app.use("/contact", express.static("src/contact"));
+
+
+// Récupérer tout un livre
+
 
 app.get("/bym/:livre", (req, res) => {
   const nom_du_livre = req.params.livre;
 
-  get_selection(res, nom_du_livre);
+  const result = get_book(nom_du_livre);
+  return_result(res, result, environment);
 })
 
 
@@ -527,7 +599,8 @@ app.get("/bym/:livre/:chap", (req, res) => {
   const nom_du_livre = req.params.livre;
   const chapitre = req.params.chap;
 
-  get_selection(res, nom_du_livre, chapitre);
+  const result = get_all_chapter(nom_du_livre, chapitre);
+  return_result(res, result, environment);
 });
 
 
@@ -537,18 +610,9 @@ app.get("/bym/:livre/:chap/:selections", (req, res) => {
   const notre_selection = req.params.selections;
   const nom_du_livre = req.params.livre;
   const num_du_chapitre = req.params.chap;
-  var v_start;
-  var v_end;
 
-  if (notre_selection.includes("-")) {
-    v_start = notre_selection.split("-")[0];
-    v_end = notre_selection.split("-")[1];
-  } else {
-    v_start = notre_selection;
-    v_end = 0;
-  }
-
-  get_selection(res, nom_du_livre, num_du_chapitre, v_start, v_end);
+  const result = get_all_of_selection(nom_du_livre, num_du_chapitre, notre_selection);
+  return_result(res, result, environment);
 });
 
 
